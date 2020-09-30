57150ce1e5d9f32ea98f363ea12d16e47a153c55
unknown <rogeriooyama@yahoo.com.br>
2020-09-30 16:20:56 -0300

    Fix

--- templates/Pages/index.php
+++ templates/Pages/index.php
@@ -42,7 +42,6 @@
         </div>
         <div class="col-lg-6 align-self-center">
           <div class="content">
-<<<<<<< Updated upstream
             <div class="row">
               <div class="mr-auto">
                 <p class="title">Natal Solidário</p>
@@ -56,13 +55,6 @@
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus accumsan tristique. Ut ornare, nisi at dignissim aliquam, justo tortor ultrices metus, a euismod orci lacus eget sem. Suspendisse eu euismod mi. Sed eget vulputate lectus, a rutrum eros. Quisque maximus dignissim purus sed commodo. Cras orci risus, mollis vel augue pulvinar, euismod volutpat velit. Morbi dictum tempus velit, vitae auctor justo congue et. Suspendisse sit amet tincidunt nisl. Donec porttitor, velit at consectetur posuere, dui diam sodales sem, ut tempus magna arcu id erat. Cras lacinia purus a sem interdum semper.
               </p>
             </div>            
-=======
-            <h1><?= $this->Html->image('bells.png', ['class' => 'mr-1', 'style' => 'height: 40px; vertical-align: text-top;']) ?>Natal Solidario</h1>
-            
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus accumsan tristique. Ut ornare, nisi at dignissim aliquam, justo tortor ultrices metus, a euismod orci lacus eget sem. Suspendisse eu euismod mi. Sed eget vulputate lectus, a rutrum eros. Quisque maximus dignissim purus sed commodo. Cras orci risus, mollis vel augue pulvinar, euismod volutpat velit. Morbi dictum tempus velit, vitae auctor justo congue et. Suspendisse sit amet tincidunt nisl. Donec porttitor, velit at consectetur posuere, dui diam sodales sem, ut tempus magna arcu id erat. Cras lacinia purus a sem interdum semper. Cras sed laoreet magna. Donec dignissim malesuada nulla, sed facilisis ligula scelerisque euismod.
-            </p>
->>>>>>> Stashed changes
           </div>
         </div>
       </div>
