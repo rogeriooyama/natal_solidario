bb4108715c2b857f3cadadc9357e3c4c73a0e223
unknown <rogeriooyama@yahoo.com.br>
2020-10-23 15:00:09 -0300

    Alteração link do logo

--- templates/layout/bootstrap.php
+++ templates/layout/bootstrap.php
@@ -39,7 +39,7 @@
   <body>
     <nav class="navbar">
       <div>        
-        <a href="<?= $this->Url->build(['controller' => 'pages', 'action' => 'index']) ?>" data-toggle="tooltip" data-placement="bottom" title="Página Inicial"><?= $this->Html->image('logo-lar-sta-filomena-branco-chapeu.png') ?></a>
+      <a href="https://larsantafilomena.org.br" data-toggle="tooltip" data-placement="bottom" title="Site Lar Santa Filomena"><?= $this->Html->image('logo-lar-sta-filomena-branco-chapeu.png') ?></a>
       </div>
       <div>
         <a class="navbar-toggler btn btn-outline-danger" href="<?= $this->Url->build(['controller' => 'users', 'action' => 'login']) ?>" data-toggle="tooltip" data-placement="left" title="Área Administrativa">
